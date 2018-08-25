c4274a116c521d54a5faf542a40b6a271d3d8d79
khegazy <kareemhegz@gmail.com>
2018-08-25 02:55:24 -0700

    Plotting class and parameters for making quick plots to illustrate results and intermediates steps of the analysis codes.

---
+++ UED/plots/scripts/plotParams.py
@@ -0,0 +1,19 @@
+
+class pltParams:
+  def __init__(self): 
+
+    self.QperPix            = 0.0223
+    self.NradAzmBins        = 555
+    self.NpairCorrAzmBins   = 398
+    self.NradLegBins        = 50
+    self.NpairCorrLegBins   = 398
+    self.Nlegendres         = 5
+
+    self.QrangeAzm = [0, QperPix*self.NpairCorrAzmBins]
+    self.RrangeAzm = [0, 6.255]
+    self.QrangeLeg = [0, QperPix*self.NradLegBins]
+    self.RrangeLeg = [0, 6]
+
+    self.smearStr  = "0.750000"
+    self.smearList = ["1.000000", "0.750000", "0.500000", "0.250000", "0.100000", "0.050000", "0.025000"]
+
