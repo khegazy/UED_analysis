4c9dfeff525b4eaa6a47b90906aebd34224b73fa
khegazy <kareemhegz@gmail.com>
2018-09-11 19:38:35 -0700

    Updated parameters.

--- UED/plots/scripts/plotParams.py
+++ UED/plots/scripts/plotParams.py
@@ -1,18 +1,20 @@
+import numpy as np
 
 class pltParams:
   def __init__(self): 
 
-    self.QperPix            = 0.0223
-    self.NradAzmBins        = 555
-    self.NpairCorrAzmBins   = 398
-    self.NradLegBins        = 50
-    self.NpairCorrLegBins   = 398
-    self.Nlegendres         = 5
+    self.Nlegendres     = 5
+    self.QperPix        = 0.0223
 
-    self.QrangeAzm = [0, self.QperPix*self.NradAzmBins]
-    self.RrangeAzm = [0, 6.255]
-    self.QrangeLeg = [0, self.QperPix*self.NradLegBins]
-    self.RrangeLeg = [0, 6]
+    self.NradAzmBins    = 555
+    self.NradLegBins    = 50
+    self.QrangeAzm      = [0, self.QperPix*self.NradAzmBins]
+    self.QrangeLeg      = [0, self.QperPix*self.NradLegBins]
+
+    self.NpairCorrBins  = 22
+    self.Rrange         = [0, 5.6354]
+    #self.Rrange         = [0, self.NpairCorrBins
+    #                       *(2*np.pi/(self.QperPix*(2*self.NradAzmBins - 1)))]
 
     self.smearStr  = "0.750000"
     self.smearList = ["1.000000", "0.750000", "0.500000", "0.250000", "0.100000", "0.050000", "0.025000"]
