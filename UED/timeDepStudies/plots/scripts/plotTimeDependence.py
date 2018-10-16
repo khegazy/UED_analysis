b6168a7ded9e6f00a674631e03c3436442e3092e
khegazy <kareemhegz@gmail.com>
2018-10-16 09:46:57 -0700

    Plot time dependence for both diffraction and real space.

--- UED/timeDepStudies/plots/scripts/plotTimeDependence.py
+++ UED/timeDepStudies/plots/scripts/plotTimeDependence.py
@@ -9,19 +9,21 @@ params = pltParams()
 plc = plotCLASS()
 
 
-runs = ["20180629_1630", "20180627_1551", "20180630_1925", "20180701_0746"]
-timeSteps = [18, 29, 19, 19]
-maxX = [1.1, 2, 1.1, 1.1]
+#runs = ["20180629_1630", "20180627_1551", "20180630_1925", "20180701_0746"]
+runs = ["20180627_1551", "20180629_1630", "20180630_1925", "20180701_0746"]
+timeSteps = [29, 18, 19, 19]
+maxX = [2, 1.1, 1.1, 1.1]
 
 
 #################################################
 #####  Plotting time dependent diffraction  #####
 #################################################
-
-colRange = [[-2e-3, 2e-3], [-2e-3, 2e-3], [-2e-3, 2e-3], [-2e-3, 2e-3]]
+#colRange = [[-2e-3, 2e-3], [-2e-3, 2e-3], [-2e-3, 2e-3], [-2e-3, 2e-3]]
+#colRange = [[-7e-3, 7e-3], [-7e-3, 7e-3], [-7e-3, 7e-3], [-7e-3, 7e-3]]
+colRange = [-3e-3, 3e-3]
 for i,run in enumerate(runs):
   opts = {
-    "colorRange" : colRange[i],
+    "colorRange" : colRange,
     "xTitle"     : "Time [ps]",
     "yTitle"     : r"Q [$\AA^{-1}$]",
     }
@@ -29,7 +31,7 @@ for i,run in enumerate(runs):
   timeDelay = np.fromfile("../../../mergeScans/results/timeDelays["
         + str(timeSteps[i] + 1) + "].dat", np.double)
   
-  plc.print2d("/reg/ued/ana/scratch/nitroBenzene/mergeScans/data-" + run + "-sMsAzmAvgDiff["
+  plc.print2d("/reg/ued/ana/scratch/nitroBenzene/mergeScans/data-" + run + "-azmAvgDiff["
         + str(timeSteps[i]) + "," + str(params.NradAzmBins) + "].dat",
         "../data-" + run + "-azmAvgDiffFull",
         X=timeDelay,
@@ -37,7 +39,7 @@ for i,run in enumerate(runs):
         options=opts)
 
   opts["xSlice"] = [-0.3, maxX[i]]
-  plc.print2d("/reg/ued/ana/scratch/nitroBenzene/mergeScans/data-" + run + "-sMsAzmAvgDiff["
+  plc.print2d("/reg/ued/ana/scratch/nitroBenzene/mergeScans/data-" + run + "-azmAvgDiff["
         + str(timeSteps[i]) + "," + str(params.NradAzmBins) + "].dat",
         "../data-" + run + "-azmAvgDiff",
         X=timeDelay,
@@ -49,18 +51,19 @@ for i,run in enumerate(runs):
 #####  Plotting time dependent pair correlation  #####
 ######################################################
 
-colRange = [[-1e-2, 1e-2], [-1e-2, 1e-2], [-1e-2, 1e-2], [-1e-2, 1e-2]]
+colRange = [-7e-2, 7e-2]#, [-1e-2, 1e-2], [-1e-2, 1e-2], [-1e-2, 1e-2]]
 for i,run in enumerate(runs):
   opts = {
-    "colorRange" : colRange[i],
+    "colorRange" : colRange,
     "xTitle"     : "Time [ps]",
     "yTitle"     : r"R [$\AA$]",
+    #"interpolate": [200, 100]
     }
 
   timeDelay = np.fromfile("../../../mergeScans/results/timeDelays["
         + str(timeSteps[i] + 1) + "].dat", np.double)
   
-  plc.print2d("../../results/data-" + run + "-pairCorr["
+  plc.print2d("../../results/data-" + run + "-pairCorrOdd["
         + str(timeSteps[i]) + "," + str(params.NpairCorrBins) + "].dat",
         "../data-" + run + "-pairCorrFull",
         X=timeDelay,
@@ -68,11 +71,14 @@ for i,run in enumerate(runs):
         options=opts)
 
   opts["xSlice"] = [-0.3, maxX[i]]
-  plc.print2d("../../results/data-" + run + "-pairCorr["
+  plc.print2d("../../results/data-" + run + "-pairCorrOdd["
         + str(timeSteps[i]) + "," + str(params.NpairCorrBins) + "].dat",
         "../data-" + run + "-pairCorr",
         X=timeDelay,
         yRange=params.Rrange,
         options=opts)
 
-
+files = ["../../results/sim-phenoxyRadical-pairCorrOdd[" + str(params.NpairCorrBins) + "].dat"]
+plc.print1d(files,
+    "../sim-phenoxyRadical-pairCorrDiff",
+    xRange=params.Rrange)
