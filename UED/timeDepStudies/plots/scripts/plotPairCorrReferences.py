a90d756ca9b0cc7fb57620bfbf0e59da8f8618e8
khegazy <kareemhegz@gmail.com>
2018-10-16 09:58:51 -0700

    Plot pair correlations of the reference images.

--- UED/timeDepStudies/plots/scripts/plotPairCorrReferences.py
+++ UED/timeDepStudies/plots/scripts/plotPairCorrReferences.py
@@ -10,25 +10,29 @@ plc = plotCLASS()
 
 
 runs = ["20180629_1630", "20180627_1551", "20180630_1925", "20180701_0746"]
-NradBins = "211"
-
-files = ["../../results/sim-simulateReference-pairCorrOdd[" + NradBins + "].dat", 
-         "/reg/ued/ana/scratch/nitroBenzene/simulations/references/nitrobenzene_pairCorr_Bins[500].dat",
-         ""]
+sims = ["nitrobenzene", "phenoxyRadical"]
 
 opts = {
   "xTitle"     : r"R [$\AA$]",
-  "yLim"       : [-0.1, 1.1],
+  #"yLim"       : [-0.1, 1.1],
   "labels"     : ["Ifft simulated sMs", "Charge weighted distances", "Ifft reference data sMs"]
 }
 
+
+files = ["../../results/sim-nitrobenzene-pairCorrOdd["\
+            + str(params.NpairCorrBins) + "].dat", 
+       "/reg/ued/ana/scratch/nitroBenzene/simulations/"\
+            + "nitrobenzene_pairCorr_Bins[500].dat",
+       ""]
 
 #######################################
 #####  Plotting pair correlation  #####
 #######################################
 
 for i,run in enumerate(runs):
-  files[2] = "../../results/data-" + run + "-reference-pairCorrOdd[" + NradBins + "].dat"
+  files[2] = "../../results/data-" + run + "-reference-pairCorrOdd["\
+      + str(params.NpairCorrBins) + "].dat"
+
 
 
   plc.print1d(files,
@@ -36,4 +40,17 @@ for i,run in enumerate(runs):
         xRange=params.Rrange,
         normalize="max",
         options=opts)
+
+  
+files = ["../../results/sim-phenoxyRadical-pairCorrOdd["\
+            + str(params.NpairCorrBins) + "].dat", 
+       "/reg/ued/ana/scratch/nitroBenzene/simulations/"\
+            + "phenoxyRadical_pairCorr_Bins[500].dat"]
+
+plc.print1d(files,
+      "../sim-phenoxyRadical-pairCorrCompare",
+      xRange=params.Rrange,
+      normalize="max",
+      options=opts)
+
 
